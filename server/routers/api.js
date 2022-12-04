14b232b88f55ff418399d3197e3bf10f63b7a145
DaDDy <blacklistuser420@gmail.com>
2022-12-04 17:45:56 +0630

    modify todo

---
+++ server/routers/api.js
@@ -0,0 +1,18 @@
+const { application } = require("express");
+const express = require("express");
+const api = express.Router();
+const loginRouter = require("./auth/login.router");
+const refreshRouter = require("./auth/refresh.router");
+const signupRouter = require("./auth/signup.router");
+const todoRouter = require("./todo/todo.router");
+const userRouter = require("./todo/users.router");
+const error = require("./auth/error.router");
+
+api.use("/", todoRouter);
+api.use("/users", userRouter);
+api.use("/login", loginRouter);
+api.use("/signup", signupRouter);
+api.use("/refresh", refreshRouter);
+api.use("/error", error);
+
+module.exports = api;
