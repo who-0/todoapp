1570c2a9953f7f916be93ef798c78d6cb5d624d4
DaDDy <blacklistuser420@gmail.com>
2022-12-13 18:20:02 +0630

    modify login controller

--- server/controllers/auth/login.controller.js
+++ server/controllers/auth/login.controller.js
@@ -5,17 +5,20 @@ const { getUserWithEmail } = require("../../models/auth/login.model");
 const httpGetUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await getUserWithEmail(email);
-    const solvepwd = await bcrypt.compare(password, user.password);
+
     if (!email && !password) {
       return res.status(400).json({
         error: "Please Input All. ",
       });
-    } else if (!user) {
+    }
+    const user = await getUserWithEmail(email);
+    if (!user) {
       return res.status(404).json({
         error: "Check your email.",
       });
-    } else if (!solvepwd) {
+    }
+    const solvepwd = await bcrypt.compare(password, user.password);
+    if (!solvepwd) {
       return res.status(404).json({
         error: "Incorrect Password. Please try again!",
       });
