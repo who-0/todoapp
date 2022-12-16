b875e28400cdb46423b0f7ff9b2bc6d54dbe1721
DaDDy <blacklistuser420@gmail.com>
2022-12-16 19:55:36 +0630

    todo ui start

--- server/controllers/todo/todo.controller.js
+++ server/controllers/todo/todo.controller.js
@@ -1,12 +1,15 @@
-const { addNewToDo } = require("../../models/todo/todo.model")
-
+const { addNewToDo } = require("../../models/todo/todo.model");
 
 const httpAddNewToDo = async (req, res) => {
-    const { id, todo } = req.body;
-    const newTodo = await addNewToDo( id, todo );
-    console.log(newTodo);
-}
+  const { id, todo } = req.body;
+  console.log(newTodo);
+};
+
+const httpGetTodo = (req, res) => {
+  const { userId } = req.body;
+  return res.render("pages/todo", { title: "ToDo", userId });
+};
 
 module.exports = {
-    httpAddNewToDo,
-}
+  httpAddNewToDo,
+};
--- server/routers/todo/todo.router.js
+++ server/routers/todo/todo.router.js
@@ -2,9 +2,30 @@ const express = require("express");
 const verifyToken = require("../../middlewares/verify.middleware");
 
 const todoRouter = express.Router();
+const todos = [
+  { todo: "you are my sunshine" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+  { todo: "you are my everything" },
+];
 
 todoRouter.get("/", verifyToken, (req, res) => {
-  res.send("This is TODO ROuter");
+  return res.render("pages/todo", {
+    title: "ToDo",
+    userId: req.data.userId,
+    todos,
+  });
+});
+
+todoRouter.post("/", verifyToken, (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = todoRouter;
